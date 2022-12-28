fb9c0e169d61d3072e12505ad7b1689211c85d56
mgynsz <112461285+mgynsz@users.noreply.github.com>
2022-12-28 13:00:43 +0900

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by mgyn on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c855856c389678c88b681657cc92f0ddf6f8465d
LimJaeHyeon9298 <limjh9298@gmail.com>
2022-12-28 13:53:15 +0900

    123

--- weatherProject/Controller/ViewController.swift
+++ weatherProject/Controller/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var jh = "임재현"
     var jh3 = "임재현"
     var jh2 = "임재현"
+    var jh3 = "임재현"
     var jy: String?
     var hs = "표현수"
     var jh: String?

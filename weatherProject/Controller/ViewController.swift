09d5f459012ea556f0125b3b14cdbc98a7a902f3
LimJaeHyeon9298 <limjh9298@gmail.com>
2022-12-28 13:24:27 +0900

    change jh

--- weatherProject/Controller/ViewController.swift
+++ weatherProject/Controller/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     //    var name: String?
     
     var jy: String?
+    var jh: String?
     //서울의 좌표
     let seoul = CLLocation(latitude: 37.5666, longitude: 126.9784)
     //날씨 데이터 저장

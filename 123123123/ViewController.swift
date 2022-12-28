b694dd6d092b1b4a12cf74284f31433ed389dd0f
LimJaeHyeon9298 <limjh9298@gmail.com>
2022-12-28 13:03:15 +0900

    Initial Commit

---
+++ 123123123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123123123
+//
+//  Created by 임재현 on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
